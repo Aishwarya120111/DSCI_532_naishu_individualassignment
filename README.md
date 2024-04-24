fa62171dedf11b85945a7f19da18c754a540fea7
Aishwarya Nadimpally <nadimpallyaishwarya@gmail.com>
2024-04-24 02:46:59 -0700

    Created Final Dashboard, Added environment file, Drafted ReadMe with Installation instructions

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # DSCI_532_naishu_individualassignment
+
+## Motivation
+
+The Aviation Atlas Dashboard is a user-friendly tool for exploring global airports. It aids decision-making in aviation, from route planning to infrastructure investment. It's easy to use and has all the info you need, making it great for everyone from industry experts to curious learners. Aviation professionals, researchers, and enthusiasts can use it to gain insights into airport distribution, types, and characteristics worldwide.
+
+## App Description
+## Installation Instructions
+To run `Aviation Atlas Dashboard` locally:
+
+1.  **Clone the Repository**
+
+    Clone the repository using Git:
+
+    ``` bash
+    git clone https://github.ubc.ca/MDS-2023-24/DSCI_532_individual-assignment_naishu.git
+    ```
+
+2.  **Navigate to the Project Directory**
+
+    Change into the project directory:
+
+    ``` bash
+    cd DSCI_532_individual-assignment_naishu
+
+    ```
+3.  **Create a Conda Environment**
+
+    Create a Conda environment named 532_HomeScope using the environment.yaml file. This file contains all necessary dependencies:
+
+    ``` bash
+    conda env create -f environment.yaml
+    ```
+
+4.  **Activate the Conda Environment**
+
+    Activate the newly created environment:
+
+    ``` bash
+    conda activate shiny_dashboard_environment
+    ```
+
+5.  **Activate R session in Bash**
+
+    Launch the R session in the environment:
+
+    ``` bash
+    r
+    ```
+
+5.  **Activate R session in Bash**
+
+    ``` bash
+    shiny::runApp('src/app.R')
+    ```
+
+This will launch the application, typically at your local web browser.
--- src/app.R
+++ src/app.R
@@ -8,30 +8,82 @@ data <- read.csv('../data/processed/airports-extended-data.csv')
 
 # Define UI for application
 ui <- fluidPage(
-  titlePanel("World Airports Tracker"),
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "custom_style.css")
+  ),
+ 
+  titlePanel("Aviation Atlas: The Global Airport Dashboard"),
+
+  # Define the sidebar layout with a row split into sidebar and main area
   sidebarLayout(
+    # Sidebar panel for inputs
     sidebarPanel(
+      img(src = "images.jpg", width = "75%",class = "centered-image"),
       selectInput("countryInput", "Select a Country:",
                   choices = sort(unique(data$country_name)),
-                  selected = c("Canada"),
-                ),
+                  selected = "Canada"),
       selectInput("provinceInput", "Select Province:",
                   choices = NULL,
-            # Initially empty, will be updated reactively
                   multiple = TRUE),
       selectInput("typeInput", "Select Type of Airport:",
-                  choice = unique(data$type),
+                  choices = unique(data$type),
                   selected = unique(data$type)[1])
-
+      
     ),
-    mainPanel(
-      plotlyOutput("mapPlot")
+
+    # Main panel for output
+    mainPanel(class = "main-panel",
+      div(class="info-box-container",
+          div(class="info-box", 
+              div(class="box-content", 
+                  span(class="box-title"),
+                  span(class="box-value", textOutput("totalCountry"))
+              )
+          ),
+          div(class="info-box", 
+              div(class="box-content", 
+                  span(class="box-title"),
+                  span(class="box-value", textOutput("totalProvince"))
+              )
+          )
+      ),
+      div(class = "custom-plotly-margin", 
+          plotlyOutput("mapPlot", height = "600px")
+      )
     )
   )
 )
 
 # Define server logic to plot the map
 server <- function(input, output, session) {
+  # Reactive expression to calculate total number of airports by country
+  totalAirportsByCountry <- reactive({
+    sum(data$country_name %in% input$countryInput & data$type == input$typeInput)
+  })
+
+  # Reactive expression to calculate total number of airports by province
+  totalAirportsByProvince <- reactive({
+    if (!is.null(input$provinceInput) && length(input$provinceInput) > 0) {
+      sum(data$country_name %in% input$countryInput & data$province %in% input$provinceInput & data$type == input$typeInput)
+    } else {
+      0  # Return 0 if no province is selected
+    }
+  })
+
+  # Render the total for country
+  output$totalCountry <- renderText({
+    paste("Number of", input$typeInput, "in", input$countryInput, ":", totalAirportsByCountry())
+  })
+
+  # Render the total for province
+  output$totalProvince <- renderText({
+    if (length(input$provinceInput) > 0) {
+      paste("Number of", input$typeInput, "in", input$provinceInput, ":", totalAirportsByProvince())
+    } else {
+      "Please select a province to see the total number of airports."
+    }
+  })
+
   observe({
     # Filter data based on selected countries
     availableProvinces <- if (is.null(input$countryInput) || length(input$countryInput) == 0) {
@@ -40,12 +92,12 @@ server <- function(input, output, session) {
       sort(unique(data[data$country_name %in% input$countryInput, "province"]))
     }
 
-    # Update cities dropdown
+    # Update provinces dropdown
     updateSelectInput(session, "provinceInput", choices = availableProvinces)
   })
 
-  # Observe country input and update types
   observe({
+    # Filter types based on selected country
     availableTypes <- if (is.null(input$countryInput) || length(input$countryInput) == 0) {
       character(0)
     } else {
@@ -61,31 +113,32 @@ server <- function(input, output, session) {
     # If no countries are selected, return a blank plot
     req(input$countryInput)
 
-    # Subset data based on selected countries, cities, and airport type
+    # Subset data based on selected inputs
     dataSubset <- subset(data, country_name %in% input$countryInput)
-
     if (!is.null(input$provinceInput) && length(input$provinceInput) > 0) {
       dataSubset <- subset(dataSubset, province %in% input$provinceInput)
     }
-
     if (!is.null(input$typeInput) && length(input$typeInput) > 0) {
       dataSubset <- subset(dataSubset, type %in% input$typeInput)
     }
 
     # Create the ggplot object with tooltips
     p <- ggplot(data = dataSubset, aes(x = longitude_deg, y = latitude_deg, text = paste("Airport:", name, "<br>Province:", province, "<br>Country:", country_name))) +
-      borders("world", colour = "gray50", fill = "gray50") +
-      geom_point(color = "red", size = 1) +
+      borders("world", colour = "#575656", fill = "#575656") +
+      geom_point(color = "skyblue", size = 1) +
       theme_minimal() +
-      ggtitle(paste("Points in", paste(input$countryInput, collapse = ", "), "-", paste(input$typeInput, collapse = ", "))) +
-      theme(legend.position = "none") # Hide legend to avoid unnecessary information
+      ggtitle(paste("Distribution of", paste(input$typeInput, collapse = ", "),"Across", paste(input$countryInput, collapse = ", "))) +
+      xlab("Longitude") +
+      ylab("Latitude") +
+      theme(legend.position = "none",plot.title = element_text(size=25, face="bold.italic"),
+axis.title.x = element_text(size=14, face="bold"),
+axis.title.y = element_text(size=14, face="bold"))
 
     # Convert to plotly object
     ggplotly(p, tooltip = "text")
   })
 
 }
-
 
 # Run the application
 shinyApp(ui = ui, server = server)
